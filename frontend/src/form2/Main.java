3e6b2e0f382b4841783c1b3bbf5c61957a355733
WallRun <dimonty2003@gmail.com>
2022-11-13 19:30:04 +0200

    Added form2

---
+++ frontend/src/form2/Main.java
@@ -0,0 +1,24 @@
+package form2;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("form2.fxml"));
+        primaryStage.setTitle("Form2");
+        Scene scene = new Scene(root, 1300, 650);
+        scene.getStylesheets().add("res/style.css");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
